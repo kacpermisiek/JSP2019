4214221f5a57f33e1c54e19cea58229ecb542676
kacpermisiek <315560@uwr.edu.pl>
2019-12-18 19:38:40 +0100

    Signed-off-by: kacpermisiek <315560@uwr.edu.pl>

--- lista9/zadanie4.py
+++ lista9/zadanie4.py
@@ -4,9 +4,15 @@ import matplotlib.pyplot as plt
 etykiety =['Java' , 'C' , 'Python' , 'C++' , 'C#' , 'VisualBasic.NET' , 'JavaScript' , 'PHP' , 'SQL' , 'Swift']
 wartosci = [17.253,16.086,10.308,6.196,4.801,4.743,2.090,2.048,1.843,1.490]
 plt.bar(etykiety,wartosci)
-plt.tick_params(labelsize=5)
+plt.tick_params(rotation=90)
+plt.subplots_adjust(bottom=0.3)
 plt.ylabel("Popularnosc [%]")
 plt.xlabel("Język")
 plt.grid(axis='y')
 plt.title("TIOBE Index for December 2019")
+
+
+
+
+
 plt.show()
